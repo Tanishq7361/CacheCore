04182679f8b5d24977f83761089f8e5fe06f454e
Tanishq7361 <tanishq7361@gmail.com>
2026-06-26 14:20:21 +0000

    feat: tokenize RESP request into lines

--- src/parser/RespParser.cpp
+++ src/parser/RespParser.cpp
@@ -1,7 +1,28 @@
 #include "../../include/parser/RespParser.h"
 
+#include <sstream>
+#include <vector>
+
 Command RespParser::parse(
     const std::string& input) const
 {
-    return {};
+    Command command;
+
+    std::istringstream stream(input);
+
+    std::vector<std::string> lines;
+
+    std::string line;
+
+    while (std::getline(stream, line))
+    {
+        if (!line.empty() && line.back() == '\r')
+        {
+            line.pop_back();
+        }
+
+        lines.push_back(line);
+    }
+
+    return command;
 }
