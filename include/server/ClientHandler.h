9a4d2094e15d79db2ae91eb01e99ee7a011a0c40
Tanishq7361 <tanishq7361@gmail.com>
2026-06-26 14:19:00 +0000

    refactor: delegate command execution to CommandExecutor

--- include/server/ClientHandler.h
+++ include/server/ClientHandler.h
@@ -2,19 +2,26 @@
 
 #include "../socket/Socket.h"
 #include "../database/Database.h"
+#include "../executor/CommandExecutor.h"
 
 class ClientHandler
 {
 private:
     Socket& socket_;
     Database& database_;
+    CommandExecutor executor_;
     int clientFd_;
 
 public:
-    ClientHandler(
+    ClientHandler::ClientHandler(
         Socket& socket,
         Database& database,
-        int clientFd);
-
+        int clientFd)
+        : socket_(socket),
+        database_(database),
+        executor_(database),
+        clientFd_(clientFd)
+    {
+    }
     void handle();
 };
