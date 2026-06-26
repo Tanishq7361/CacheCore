3ddb2d87028bfdb4bf34d5686c0d4bc84fe7dbe1
Tanishq7361 <tanishq7361@gmail.com>
2026-06-26 14:20:24 +0000

    feat: parse RESP commands into Command objects

--- src/parser/RespParser.cpp
+++ src/parser/RespParser.cpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <vector>
+#include <stdexcept>
 
 Command RespParser::parse(
     const std::string& input) const
@@ -22,6 +23,28 @@ Command RespParser::parse(
         }
 
         lines.push_back(line);
+    }
+
+    if (lines.empty())
+    {
+        return command;
+    }
+
+    if (lines[0].empty() || lines[0][0] != '*')
+    {
+        throw std::runtime_error("Invalid RESP array");
+    }
+
+    for (std::size_t i = 2; i < lines.size(); i += 2)
+    {
+        if (i == 2)
+        {
+            command.name = lines[i];
+        }
+        else
+        {
+            command.arguments.push_back(lines[i]);
+        }
     }
 
     return command;
