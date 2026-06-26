893afcc59865ee6e69cceaca99cd4024d342b8d2
Tanishq7361 <tanishq7361@gmail.com>
2026-06-26 14:18:57 +0000

    refactor: move command execution into CommandExecutor

---
+++ src/executor/CommandExecutor.cpp
@@ -0,0 +1,96 @@
+#include "../../include/executor/CommandExecutor.h"
+
+CommandExecutor::CommandExecutor(Database& database)
+    : database_(database)
+{
+}
+
+std::string CommandExecutor::execute(const Command& command)
+{
+    if (command.name == "SET")
+    {
+        if (command.arguments.size() != 2)
+        {
+            return "ERR invalid SET command\n";
+        }
+
+        database_.set(
+            command.arguments[0],
+            command.arguments[1]);
+
+        return "OK\n";
+    }
+
+    else if (command.name == "GET")
+    {
+        if (command.arguments.size() != 1)
+        {
+            return "ERR invalid GET command\n";
+        }
+
+        return database_.get(command.arguments[0]) + "\n";
+    }
+
+    else if (command.name == "DEL")
+    {
+        if (command.arguments.size() != 1)
+        {
+            return "ERR invalid DEL command\n";
+        }
+
+        return database_.del(command.arguments[0])
+               ? "1\n"
+               : "0\n";
+    }
+
+    else if (command.name == "EXISTS")
+    {
+        if (command.arguments.size() != 1)
+        {
+            return "ERR invalid EXISTS command\n";
+        }
+
+        return database_.exists(command.arguments[0])
+               ? "1\n"
+               : "0\n";
+    }
+
+    else if (command.name == "KEYS")
+    {
+        if (!command.arguments.empty())
+        {
+            return "ERR KEYS takes no arguments\n";
+        }
+
+        auto keys = database_.keys();
+
+        if (keys.empty())
+        {
+            return "(empty)\n";
+        }
+
+        std::string response;
+
+        for (const auto& key : keys)
+        {
+            response += key;
+            response += '\n';
+        }
+
+        return response;
+    }
+
+    else if (command.name == "CLEAR")
+    {
+        if (!command.arguments.empty())
+        {
+            return "ERR CLEAR takes no arguments\n";
+        }
+
+        database_.clear();
+
+        return "OK\n";
+    }
+
+    return "ERR unknown command\n";
+}
