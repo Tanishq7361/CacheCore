3a203a3ec4c56ac80a9130cad000d29244b06104
Tanishq7361 <tanishq7361@gmail.com>
2026-06-26 14:19:06 +0000

    refactor: extend database entries with expiration metadata

--- include/database/Entry.h
+++ include/database/Entry.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <chrono>
 #include <string>
 
 struct Entry
 {
     std::string value;
+
+    bool hasExpiry = false;
+
+    std::chrono::steady_clock::time_point expiryTime;
 };
